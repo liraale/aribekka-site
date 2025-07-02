7bcd7fa2a175b60c044d7ef706c7bca940939da4
liraale <liraale@gmail.com>
2025-07-02 10:53:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
-const radius = 150;
-const tags = document.querySelectorAll('#tag-sphere li');
-const total = tags.length;
+const canvas = document.getElementById('sphere-canvar');
+const ctx = canvas.getContext('2d');
+const width	= canvas.width;
+const radius = 200;
+const center = { x: width / 2, y: height / 2 };
 
-tags.forEach((tag, i) => {
-  const angle = (i / total) * Math.PI * 2;
-  const x = radius * Math.cos(angle);
-  const y = radius * Math.sin(angle);
-  tag.style.transform = `translate3d(${x}px, ${y}px, 0)`;
-});
+const topics = [
+  { label: 'Apple', url: 'https://www.apple.com' },
+  { label: 'Microsoft', url: 'https://www.micrsoft.com' },
+  { label: 'Android', url: 'https://www.android.com' },
+  { label: 'Asus', url: 'https://www.asus.com' },
+  { label: 'Xbox', url: 'https://www.xbox.com' },
+  { label: 'Playstation', url: 'https://www.playstation.com' },
+  { label: 'Nintendo', url: 'https://www.nintendo.com' },
+  { label: 'VR', url: '#' },
+  { label: 'Audio', url: '#' },
+  { label: 'Impresión', url: '#' },
+  { label: '3D Print', url: '#' }
+  ];
+
+  const points = topics.map((topic, i) => {
+  	const phi = Math.acos(-1 + (2 * i) / topics.length);
+  	const theta = Math.sqrt(topics.length * Math.PI) * phi;
+  	return {
+  		x: radius * Math.cos(theta) * Math.sin(phi),
+  		y: radius * Math.sin(theta) * Math.sin(phi),
+  		z: radius * Math.cos(phi),
+  		label: topic.label,
+  		url: topic.url
+  	};
+  });
+
+  function draw() {
+  	ctx.clearRect(0, 0, width, height);
+
+  	//Dibujar lineas entre nodos
+  	ctx.strokeStyle = 'rgba(0, 234, 255, 0.2)';
+  	points.forEach((p1, i) => {
+  		for (let j = i +1; j < points.length; j++) {
+  			const p2 = points[j];
+  			ctx.beginPath();
+  			ctx.moveTo(center.x + p1.x, center.y + p1.y);
+  			ctx.lineTo(center.x + p2.x, center.y + p2.y);
+  			ctx.stroke();
+  		}
+  	});
+
+  	//Dibujar Nodos
+  	points.forEach(p => {
+  		ctx.beginPath();
+  		ctx.arc(center.x +p.x, center.y + p.y, 6, 0, 2 * Math.PI);
+  		ctx.fillStyle = '#00eaff';
+  	});
+  }
+
+  draw();
