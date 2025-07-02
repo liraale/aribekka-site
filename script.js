7c6b0afb4ef1ec7b7aec39c1a9e856048de08377
liraale <liraale@gmail.com>
2025-07-02 12:43:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('sphere-canvas');
 const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(0, 0, 100, 100);
 const width	= canvas.width;
 const height = canvas.height;
 const radius = 200;
